1d57389a267e7003973e1908468a4645cdaa6d0e
BOTJEFFreal <74595339+BOTJEFFreal@users.noreply.github.com>
2023-10-22 02:15:47 +0530

    fixed import error

--- Backend/chatApp/views.py
+++ Backend/chatApp/views.py
@@ -1,4 +1,4 @@
-from Backend.chatApp.sentiment_model import get_Sentiment
+from .sentiment_model import get_Sentiment
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
