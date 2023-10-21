6b6dcfcdbbbf0ffc54cdf693e59d98a205b4f019
YashKandoi <yashkandoi2003@gmail.com>
2023-10-22 04:09:39 +0530

    Changes done for implementing qr code scanner

--- ClientSide/lib/main.dart
+++ ClientSide/lib/main.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:hrchatbot/pages/chatPage.dart';
 import 'package:hrchatbot/pages/form.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  late QRViewController controller;
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      if (scanData != null && scanData.isNotEmpty) {
+        if (scanData == '123456') {
+          Navigator.push(
+            qrKey.currentContext,
+            MaterialPageRoute(builder: (context) => RatingForm()),
+          );
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: RatingForm(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('QR Code Scanner'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: QRView(
+                key: qrKey,
+                onQRViewCreated: _onQRViewCreated,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
-
