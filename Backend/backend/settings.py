5c27a4f1456ce767d9b2cea02c32956a657dfc70
BOTJEFFreal <74595339+BOTJEFFreal@users.noreply.github.com>
2023-10-22 10:06:51 +0530

    added OPEN_API_KEY

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,6 +84,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 DATABASES = {
     'default': {
